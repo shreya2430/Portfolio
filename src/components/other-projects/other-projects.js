4b9dcfd79adb6e2e1c2e68528efabb54d505ec87
amoddhopavkar2 <dhopavkar.am@northeastern.edu>
2024-03-03 03:17:18 -0500

    add projects

--- src/components/other-projects/other-projects.js
+++ src/components/other-projects/other-projects.js
@@ -6,7 +6,7 @@ const otherProjectsData = [
   {
     "name": "Kitchen Chronicles",
     "url": "https://leafy-gumption-a6a15e.netlify.app/",
-    "description": "I developed and launched a full-stack web application. I implemented a smooth user interface with features like searching and pagination. I built a database and API server to store and access data. I designed a responsive interface for seamless mobile and desktop use. I enabled secure user authentication to protect certain app features. I leveraged state management and routing tools for efficient data flow. I deployed the front and back-end on cloud platforms to make the site live and accessible. <br> <a class='small' href='https://github.com/amoddhopavkar2/kitchen-chronicles-react-app'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
+    "description": "I built and launched the Kitchen Chronicles web app to showcase my expertise across modern frameworks. I crafted an intuitive UI with React.js featuring smooth search and pagination. Leveraging MongoDB and Express.js, I developed a scalable cloud database and REST API backend enabling secure access to recipe data. To manage complex state, I applied Redux Hooks for seamless data flow. For UX, my responsive design enabled mobile and desktop ease-of-use. By integrating JWT authentication, I enabled key feature protection, applying industry security norms. Deployed on Netlify and Render, this project highlights my adeptness with high-performance stacks to construct consumer web apps with appealing design, protected access, and optimized data loading. <br> <a class='small' href='https://github.com/amoddhopavkar2/kitchen-chronicles-react-app'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
     "used": [
       {"thing": "MongoDB"},
       {"thing": "Node.js"},
@@ -19,104 +19,96 @@ const otherProjectsData = [
     ]
   },
   {
-    "name": "Covidstat",
-    "url": "https://www.linkedin.com/posts/kavin-varnan-08083229_covid-covid19-dashboard-activity-6668032584783663104-cRzB?utm_source=share&utm_medium=member_desktop",
-    "description": "I've developed an elegant web application that displays COVID-19 statistics. The website dynamically retrieves data from reputable sources such as John Hopkins University and government websites to ensure accuracy. What sets it apart is the inclusion of a <a class='underline-link' href='https://graphql.org'>GraphQL</a> endpoint, providing a public API for seamless data consumption by anyone interested. Explore the API Documentation for further details. This comprehensive tool not only offers insightful statistics but also demonstrates a commitment to transparency and accessibility in sharing crucial information about the pandemic. <br> <a class='small' href='https://github.com/COVID19-SARS-CoV-2/web-covid-api'>👨🏻‍💻API Documentation👨🏻‍💻</a>",
+    "name": "Inventory Management System",
+    "description": "I engineered an Inventory Management web application using a robust cloud technology stack. Developing the front-end UI with React.js allowed me to create highly responsive and intuitive inventory dashboards. By leveraging Node.js with Express.js for the back-end APIs, I provided robust real-time inventory tracking and order management functionalities. Further, hosting the PostgreSQL database on Amazon RDS optimized infrastructure scalability and reliability. My expertise across the full-stack along with the leveraging of AWS's managed database services led to an Inventory Management System with enterprise-grade performance levels. This project highlights my systems design capabilities spanning UI development, cloud APIs, and choice of efficient data persistence layers.",
     "used": [
       {"thing": "Node.js"},
-      {"thing": "Docker"},
-      {"thing": "MongoDB"},
-      {"thing": "GraphQL"},
-      {"thing": "Redis"},
-      {"thing": "Digital Ocean"},
-      {"thing": "API"},
-      {"thing": "Open-Source"},
-      {"thing": "React"},
-    ]
-  },
-  {
-    "name": "Gallery For Lollipop",
-    "url": "https://play.google.com/store/apps/details?id=com.gallery.lollipop",
-    "description": "When Google introduced Google Photos, they unexpectedly removed the widely-used Gallery app from Android with the release of Android Lollipop. Leveraging the open-source code from Android, I took it upon myself to recreate and revive the Gallery app, ensuring users could continue to enjoy a familiar and essential feature. Now, you can experience the convenience and functionality of the Gallery app once again, thanks to this recreation.",
-    "used": [
-      {"thing": "Android Native"},
-      {"thing": "Java"},
-      {"thing": "Open-Source"},
-      {"thing": "AdMob"},
-    ]
-  },
-  {
-    "name": "NHS Covid App",
-    "url": "https://x.com/skvarnan/status/1242406271942144000",
-    "description": "At the onset of the pandemic, widespread panic ensued, and there was a noticeable absence of readily accessible guidelines. In collaboration with the NHS, I contributed to the creation of an app designed to streamline the dissemination of crucial guidelines. This innovative solution not only facilitated easy updates for the NHS but also addressed the challenge of limited internet connectivity within hospitals. The app's offline functionality ensured that vital information remained accessible, even in areas with restricted internet access, supporting healthcare professionals in their critical work during challenging circumstances. <br> <a class='small' href='https://github.com/skavinvarnan/nhs-covid-flutter'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
-    "used": [
-      {"thing": "Android Native"},
-      {"thing": "iOS Native"},
-      {"thing": "Node.js"},
-      {"thing": "Strapi"},
-      {"thing": "MongoDB"},
-      {"thing": "Docker"},
-      {"thing": "GraphQL"},
-      {"thing": "Digital Ocean"},
-      {"thing": "Swift"},
-      {"thing": "Kotlin"},
-    ]
-  },
-  {
-    "name": "CrossPlatform AES",
-    "url": "https://github.com/skavinvarnan/Cross-Platform-AES",
-    "description": "I've developed an open-source library for AES-256 encryption and decryption of strings, designed to seamlessly integrate across Android, iOS, and Javascript platforms. This versatile library enhances data security, offering a robust solution for safeguarding sensitive information. Whether you're working on Android, iOS, or web applications, this library provides a reliable and consistent encryption and decryption mechanism for strings, ensuring data confidentiality and integrity in your projects. <br> <a class='small' href='https://github.com/skavinvarnan/Cross-Platform-AES'>🏆 100+ Starts on Github 🏆</a>",
-    "used": [
-      {"thing": "Kotlin"},
-      {"thing": "Swift"},
-      {"thing": "JavaScript"},
-    ]
-  },
-  {
-    "name": "Social Evening",
-    "url": "https://github.com/skavinvarnan/SocialEvening",
-    "description": "This was an interview project<br><br>I successfully developed and documented the entire app in an impressive timeframe of less than seven days. This rapid turnaround not only showcases my efficiency in app development but also underscores the meticulous attention given to detailed documentation. The result is a well-crafted application, ready to make an impact with its functionality and comprehensive supporting documentation. <br> <a class='small' href='https://github.com/skavinvarnan/SocialEvening'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
-    "img": "weather",
-    "used": [
-      {"thing": "Android Native"},
-      {"thing": "Kotlin"},
-      {"thing": "Node.js"},
-      {"thing": "Parse Server"},
+      {"thing": "Express.js"},
+      {"thing": "React.js"},
+      {"thing": "Amazon Web Services (AWS)"},
+      {"thing": "AWS - RDS"},
       {"thing": "PostgreSQL"},
-      {"thing": "Heroku"},
     ]
   },
   {
-    "name": "2048 Terminal",
-    "url": "https://github.com/skavinvarnan/2048",
-    "description": "Experience a captivating 2048 game right in your terminal. Immerse yourself in the challenge of this numeric puzzle, all within the simplicity of a command-line interface. Enjoy the strategic thrill as you merge tiles to reach the elusive 2048. Ready for a brain-teasing adventure? Play the terminal-based 2048 game now!. <br> <a class='small' href='https://github.com/skavinvarnan/2048'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
+    "name": "MoneyMagnet: Stock Market Portfolio Management & Simulation",
+    "description": "I developed a Java-based desktop application following the MVC design pattern, enabling advanced portfolio management and simulation. I engineered a user-friendly platform that offers real-time stock portfolio management, providing investors with invaluable insights and tools for informed decision-making. I seamlessly integrated the Alphavantage API into the application, ensuring access to precise and up-to-date stock data, enhancing the overall functionality and accuracy of portfolio management. I facilitated data-driven investment strategies by creating a dynamic interface that allows users to track their investments, assess market trends, and optimize their portfolios.",
     "used": [
-      {"thing": "Shell"},
-      {"thing": "JavaScript"},
+      {"thing": "Java"},
+      {"thing": "Maven"},
+      {"thing": "Java - Swing"},
+      {"thing": "REST APIs"},
+      {"thing": "Model View Controller (MVC)"},
+      {"thing": "Pair Programming"},
+      {"thing": "Software Design Patterns"},
+      {"thing": "API Integration & Testing"},
     ]
   },
   {
-    "name": "CIC Calculator",
-    "url": "https://github.com/skavinvarnan/cic-flutter",
-    "description": "Explore an easy-to-use compound interest calculator. This straightforward tool allows you to effortlessly calculate compound interest, making financial planning a breeze. Crunch numbers with simplicity and precision to get a clear understanding of your savings and investments. Try out our user-friendly compound interest calculator for hassle-free financial calculations! <br> <a class='small' href='https://github.com/skavinvarnan/cic-flutter'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
+    "name": "Packet Tracer for the Distributed Replicated Block Device (DRBD)",
+    "description": "As part of my collaboration with Veritas, I enhanced the open-source Distributed Replicated Block Device by creating a custom packet tracer within its kernel module to collect invaluable performance data. By programming the module in C and integrating debugging tools, I gained deep visibility into packet-level operations, enabling a 22% boost in platform performance. Further, by applying a Binary Classifier machine learning model to identify potential outages from packet patterns, I reduced active downtime by 36%. My kernel programming and ML innovations unlocked actionable analytics, optimized reliability, and highlighted impactful innovation on a widely adopted open-source project.",
     "used": [
-      {"thing": "Flutter"},
-      {"thing": "Dart"},
-      {"thing": "Android"},
-      {"thing": "iOS"},
-      {"thing": "Web"},
+      {"thing": "Kernel Programming"},
+      {"thing": "C"},
+      {"thing": "Open-Source"},
+      {"thing": "CScope"},
+      {"thing": "Machine Learning"},
+      {"thing": "Binary Classifier"},
     ]
   },
   {
-    "name": "Personal Website V1",
-    "url": "https://github.com/skavinvarnan/skvarnan",
-    "description": "Craft a stunning personal website that highlights your portfolio and achievements. Showcase your work in a professionally designed space tailored to reflect your unique talents. Let's build an online presence that leaves a lasting impression. Elevate your personal brand with a website that speaks volumes about your skills and accomplishments. <br> <a class='small' href='https://github.com/skavinvarnan/skvarnan'>👨🏻‍💻Github Source Code👨🏻‍💻</a>",
+    "name": "COVID-19 Face Mask & Temperature Detection System",
+    "description": "The COVID-19 Face Mask & Temperature Detection System uses OpenCV and Python for real-time enforcement of COVID safety protocols. The system leverages computer vision to detect if people are wearing masks over their mouths and noses. Additionally, it estimates facial skin temperature to identify potential fevers. If mask or fever violations are detected, alerts are triggered for administrative intervention. Built using OpenCV and PyTorch libraries, this system enables automated compliance monitoring to minimize virus transmission by instantly flagging risky cases for further screening. The key benefits are rapid detection capabilities, minimization of human oversight needs, and scalable deployment options for diverse locations. Overall, the system combines efficiency and public health safeguarding to enforce safety amid the challenges of an ongoing pandemic.",
     "used": [
-      {"thing": "React"},
-      {"thing": "Gastby"},
-      {"thing": "JavaScript"},
+      {"thing": "Python"},
+      {"thing": "OpenCV"},
+      {"thing": "Pytorch"},
+      {"thing": "Computer Vision"},
     ]
-  }
+  },
+  {
+    "name": "Automated Credit Risk Analysis",
+    "description": "The Automated Credit Risk Analysis system utilizes a Random Forest Classifier machine learning model to automate credit risk assessment, eliminating the need for manual verification. Developed in Python using libraries like Pandas, NumPy and Scikit-Learn, the system extracts applicant financial data, builds a classification algorithm with decision trees, and predicts likely loan defaults with up to 95% accuracy. Core benefits are faster loan approvals, lowered risk, and quick auto-generation of credit analysis reports for stakeholders. This system accelerates processes through data-based automation, allowing human efforts to focus on higher complexity tasks.",
+    "used": [
+      {"thing": "Python"},
+      {"thing": "Numpy"},
+      {"thing": "Pandas"},
+      {"thing": "Machine Learning"},
+      {"thing": "Random Forest Classifier"},
+      {"thing": "Scikit-Learn"},
+      {"thing": "Decision Trees"},
+    ]
+  },
+  {
+    "name": "Online Library Management System",
+    "description": "The Online Library Management System is a full-stack web application developed to digitize and automate a university library's book management operations. Core functionalities include tracking current inventory across catalogues, issuing and returning books, along with member and subscription management. Built using MongoDB, Express.js, AngularJS, Node.js and REST APIs, this system enables seamless book record-keeping, transaction logs, real-time book availability checks, and analytics reports to optimize library collections. Key benefits are automation of manual processes, improved accuracy of book histories, and enhanced accessibility to students and librarians by providing self-service online interfaces.",
+    "used": [
+      {"thing": "MongoDB"},
+      {"thing": "AngularJS"},
+      {"thing": "Express.js"},
+      {"thing": "Node.js"},
+      {"thing": "REST APIs"},
+    ]
+  },
+  {
+    "name": "Smart Tax Rebate Calculator",
+    "description": "The Smart Tax Rebate Calculator is a web application that streamlines tax computations for products by automatically calculating federal, state, and local tax rates using integrated databases. Its key innovation is the intelligent computation of applicable tax rebates for each product based on rebate histories to maximize deductions. By leveraging rebate analytics and automated rate calculations, this application reduces manual computation work by over 50+ hours each cycle, enabling finance teams to focus on value-add analysis. The tool provides accurate, automated tax calculations while optimizing eligible tax rebates.",
+    "used": [
+      {"thing": "MySQL"},
+      {"thing": "HTML"},
+      {"thing": "CSS"},
+      {"thing": "Bootstrap"},
+    ]
+  },
+  {
+    "name": "Twitter Bot",
+    "description": "I leveraged my expertise in API integration to create an intelligent Twitter Bot. Using the Twitter API and tweepy library, I designed a convenient bot interface accepting keyword and tweet count inputs. Integrating seamlessly via secure API keys, my programmatic solution identifies the latest relevant tweets and efficiently likes them on the user's behalf.",
+    "used": [
+      {"thing": "Python"},
+      {"thing": "API Integration"},
+      {"thing": "API Testing"},
+    ]
+  },
 ]
 
 const FeaturedProjects = () => (
