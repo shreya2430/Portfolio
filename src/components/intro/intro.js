46f0c7596d9893b344d678e5a9433c8dac76da1f
amoddhopavkar2 <dhopavkar.am@northeastern.edu>
2024-08-22 23:45:24 -0400

    add profile picture

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import "./intro.scss"
-
+import profileImage from "../../img/profile.jpg"
 
 const introData = {
   title: "Hello! ",
@@ -10,7 +10,9 @@ const introData = {
   contact: "Get in touch ",
   email: "dhopavkar.am@northeastern.edu",
   mailTo: "mailto:dhopavkar.am@northeastern.edu",
+  profileImage: profileImage,
 };
+
 const Intro = () => {
   const [isHovering, setIsHovering] = React.useState(false);
   const onMouseOver = _ => {
@@ -29,28 +31,35 @@ const Intro = () => {
     }, 1000);
   }, []);
 
-  return <header className="intro">
-    <h1 className="intro__hello">{introData.title}
-      <span className={isHovering ? "emoji wave-hand animated wave" : "emoji wave-hand animated"} onMouseOver={onMouseOver} onMouseOut={onMouseOut} onFocus={onMouseOver} onBlur={onMouseOut} role="button" tabIndex={0} aria-label="wave hand"></span>
-    </h1>
+  return (
+    <header className="intro">
+      <div className="intro__content">
+        <div className="intro__text">
+          <h1 className="intro__hello">{introData.title}
+            <span className={isHovering ? "emoji wave-hand animated wave" : "emoji wave-hand animated"} onMouseOver={onMouseOver} onMouseOut={onMouseOut} onFocus={onMouseOver} onBlur={onMouseOut} role="button" tabIndex={0} aria-label="wave hand"></span>
+          </h1>
 
-    <h2 className="intro__tagline">{introData.beforeName}
-      <span className="name">{introData.name}</span>{introData.afterName}
-      <span className="emoji technologist"></span>
-    </h2>
+          <h2 className="intro__tagline">{introData.beforeName}
+            <span className="name">{introData.name}</span>{introData.afterName}
+            <span className="emoji technologist"></span>
+          </h2>
 
-    <h3 className="intro__contact">
-      <a href="./resume.pdf" target="_blank" rel="noreferrer" className="arrow-link">View My Resume</a>
-      <br></br>
-      <span>{introData.contact}</span>
-      <span className="emoji pointer"></span>
-      <span>
-      <a href={introData.mailTo}
-         className="highlight-link">{introData.email}</a>
-    </span>
-
-    </h3>
-  </header>
+          <h3 className="intro__contact">
+            <a href="./resume.pdf" target="_blank" rel="noreferrer" className="arrow-link">View My Resume</a>
+            <br />
+            <span>{introData.contact}</span>
+            <span className="emoji pointer"></span>
+            <span>
+              <a href={introData.mailTo} className="highlight-link">{introData.email}</a>
+            </span>
+          </h3>
+        </div>
+        <div className="intro__image">
+          <img src={profileImage} alt="Amod Dhopavkar" className="profile-image" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Intro
